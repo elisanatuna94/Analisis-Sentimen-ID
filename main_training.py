f62a717cc5300ee22285d24bd1adf638d8d30ef1
elisanatuna94 <55584223+elisanatuna94@users.noreply.github.com>
2019-09-20 17:45:01 +0700

    Update main_training.py

--- main_training.py
+++ main_training.py
@@ -6,7 +6,7 @@ an = Analiser(training_data='data/training_all_random.csv')
 # train new model
 an.train(filename='model')
 
-test = "ahok itu pemimpin yang beres memimpin"
+test = "Sbg mantan wartawan Tempo yg sudah lebih dari 10 tahun di luar kerja jurnalisme majalah itu, saya merasa bisa ambil jarak dari padanya.  Tempo bisa keliru, bisa punya bias, tapi saya tahu dlm kritiknya tak terkandung sikap a priori anti "
 print test
 print an.testFromTrained([an.tfidf_data.transform(test)])
 
