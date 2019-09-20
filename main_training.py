42e335a437376df14baa07a6a48a2b45083f8b19
elisanatuna94 <55584223+elisanatuna94@users.noreply.github.com>
2019-09-20 17:54:24 +0700

    Update main_training.py

--- main_training.py
+++ main_training.py
@@ -6,10 +6,10 @@ an = Analiser(training_data='data/training_all_random.csv')
 # train new model
 an.train(filename='model')
 
-test = "ahok itu pemimpin yang beres memimpin"
+test = "Kemarin jumpa tak sengaja @sahaL_AS. Masih ingat katakata dia: kalau RKUHP disahkan, tak akan bisa mundur lagi keadaan menjadi spt sebelumnya. Harus dicegah. Moral force harus bangkit. Mahasiswa yg bergerak perlu dikawal semua elemen. "
 print test
 print an.testFromTrained([an.tfidf_data.transform(test)])
 
-test = "ahok itu pemimpin yang ga beres memimpin"
+test = "Ketika Habibie memimpin ICMI (Ikatan Cendekiawan Muslim Indonesia), banyak yg takut, atau sebaliknya berharap,   mereka akan memberi hak istimewa kpd elite Muslim. Ternyata tidak. Habibie dan banyak tokoh ICMI adalah pendukung kebhinekaan.#habibie"
 print test
 print an.testFromTrained([an.tfidf_data.transform(test)])
